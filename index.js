3fae808ddfb5a21315d4f3be6e488da8c3470a08
Yurii <donnikrvav@gmail.com>
2020-12-28 22:30:47 +0200

    new 2 commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const user = localStorage.getItem('user')
+
 const pass = localStorage.getItem('password')
+
+const [messageElem, signInButton, signUpButton] = ['message', 'signIn', 'signUp']
+  .map((id) => document.getElementById(id))
+
 fetch(`http://localhost:3000/users?login=${user}`)
   .then((response) => response.json())
   .then((response) => {
     if (response[0].password === pass) {
-      document.getElementById('message').innerHTML = '<h1>Hello</h1>'
+      messageElem.innerHTML = '<h1>Hello</h1>'
     } else {
-        document.getElementById('message').innerHTML = '<h1>you should login or register</h1>'
+        messageElem.innerHTML = '<h1>you should login or register</h1>'
+        signInButton.disabled = false
+        signUpButton.disabled = false
     }     
   })
+
+signUpButton.onclick = function(event) {
+  const script = document.body.appendChild(document.createElement('script'))
+  script.src = './signup.js'
+}
