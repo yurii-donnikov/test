f1b1c988bc8eca8a14c14dc0ef3f9ddac1d88692
Yurii <donnikrvav@gmail.com>
2021-01-01 13:27:42 +0200

    01.01 13:27

--- signin.js
+++ signin.js
@@ -3,7 +3,6 @@ const loadformIn = async () => {
     document.body
       .appendChild(document.createElement('main'))
         .innerHTML = html
-          .id = 'main1'
   const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture']
     .map((id) => document.getElementById(id))
   
