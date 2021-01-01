2b8fceafac5f987d76139dd33e02ef961e791aab
Yurii <donnikrvav@gmail.com>
2021-01-01 13:23:40 +0200

    01.01 13:23

--- signin.js
+++ signin.js
@@ -1,8 +1,9 @@
 const loadformIn = async () => { 
   const html = await (await fetch('https://yurii-donnikov.github.io/test/resources/signup.html')).text()
     document.body
-      .appendChild(document.createElement('main')).id = 'main1'
+      .appendChild(document.createElement('main'))
         .innerHTML = html
+          .id = 'main1'
   const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture']
     .map((id) => document.getElementById(id))
   
