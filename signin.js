4dd0af94aa8c0bc77279c45f97084f76daf49710
Yurii <donnikrvav@gmail.com>
2021-01-02 17:35:48 +0200

    02.01 17:35

--- signin.js
+++ signin.js
@@ -6,63 +6,26 @@ const loadformIn = async () => {
     document.body
         .appendChild(document.createElement('main'))
         .innerHTML = html
-    const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture']
+    const [login, password, avatar, submit, picture] = ['login', 'passwordIn', 'avatar', 'submitIn', 'pictureIn']
     .map((id) => document.getElementById(id))
 
-    //    login.onchange = function (event) {
-    //        fetch(`http://localhost:3000/users?login=${event.target.value}`)
-    //            .then((response) => response.json())
-    //            .then((response) => {
-    //                if (response.length > 0) {
-    //                    event.target.style.color = 'green'
-    //                    password.disabled = false
-    //                    //                        localStorage.setItem('user', event.target.value)
-    //                } else {
-    //                    event.target.style.color = 'red'
-    //                    password.disabled = true
-    //                }
-    //            })
-    //    }
-
-    //    login.onchange = function (event) {event.target.value}
-    //
-    //    password.onchange = function (event) {
-    //        event.target.value
-    //    if (event.target.value.length >= 8 && event.target.value.match(/[\dA-Z]+/g)) {
-    //      avatar.disabled = false
-    //      submit.disabled = false
-    //      const hash = Sha256.hash(event.target.value)
-    //      localStorage.setItem('pass', hash)
-    //    }
-    //    }
     var log = ''
     var myLog = login.onchange = function (event) {
         log = event.target.value
     }
-    myLog
 
     var hashn = ''
     var myPass = password.onchange = function (event) {
         hashn = Sha256.hash(event.target.value)
     }
 
-
-
-    submit.disabled = false
-    password.disabled = false
-
     submit.onclick = function (event) {
         fetch(`http://localhost:3000/users?login=${log}`)
             .then((response) => response.json())
-            .then((resp) => 
+            .then((resp) =>
                 resp[0].password === hashn ?
-                    document.getElementById('picture').src = resp[0].avatar : console.warn('неверный пароль')
-
+                document.getElementById('pictureIn').src = resp[0].avatar : console.warn('неверный пароль')
             )
-
-
-        //                  === myPass 
-        //                console.log('все верно') : console.log('пароль не верный'))
     }
 }
 
