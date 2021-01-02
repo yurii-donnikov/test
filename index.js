32c46c3238bb81a2bbff4a075cde0aeebda7102a
Yurii <donnikrvav@gmail.com>
2021-01-02 19:58:05 +0200

    02.01 19:58

--- index.js
+++ index.js
@@ -3,26 +3,30 @@ const user = localStorage.getItem('user')
 const pass = localStorage.getItem('password')
 
 const [messageElem, signInButton, signUpButton] = ['message', 'signIn', 'signUp']
-  .map((id) => document.getElementById(id))
+.map((id) => document.getElementById(id))
 
 fetch(`http://localhost:3000/users?login=${user}`)
-  .then((response) => response.json())
-  .then((response) => {
-    if (response[0].password === pass) {
-      messageElem.innerHTML = '<h1>Hello</h1>'
-    } else {
-        messageElem.innerHTML = '<h1> you should login or register </h1>'
-        signInButton.disabled = false
-        signUpButton.disabled = false
-    }     
-  })
+    .then((response) => response.json())
+    .then((response) => {
+        if (response[0].password === pass) {
+            messageElem.innerHTML = '<h1>Hello</h1>'
+        } else {
+            messageElem.innerHTML = '<h1> you should login or register </h1>'
+            signInButton.disabled = false
+            signUpButton.disabled = false
+        }
+    })
 
-signUpButton.onclick = function(event) {
-  const script = document.body.appendChild(document.createElement('script'))
-  script.src = './signup.js'
+signUpButton.onclick = function (event) {
+    location.reload()
+
+    const script = document.body.appendChild(document.createElement('script'))
+    script.src = './signup.js'
 }
 
-signInButton.onclick = function(event) {
-  const script = document.body.appendChild(document.createElement('script'))
-  script.src = './signin.js'
+signInButton.onclick = function (event) {
+    location.reload()
+
+    const script = document.body.appendChild(document.createElement('script'))
+    script.src = './signin.js'
 }
