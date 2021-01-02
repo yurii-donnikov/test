9d1cb6de7ea40c9eca8cc29b0b73963710a58699
Yurii <donnikrvav@gmail.com>
2021-01-02 16:48:46 +0200

    02.01 16:48

--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
-//const script = document.body.appendChild(document.createElement('script'))
-//
+const script = document.body.appendChild(document.createElement('script'))
+
 script.onload = function() {
   console.log(Sha256)
 }
@@ -7,8 +7,8 @@ script.onload = function() {
 script.onerror = function(event){
   console.log(event)
 }
-//
-//script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
+
+script.src = "https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js"
 
 const loadform = async () => { 
   const html = await (await fetch('https://yurii-donnikov.github.io/test/resources/signup.html')).text()
